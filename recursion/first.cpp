e71743ad64485a1b6d3f44c5f8d3378a1da3845a
Aakash Goswami <aakash.goswami2244@gmail.com>
2023-01-04 19:10:42 +0530

    add parameterize function

--- recursion/first.cpp
+++ recursion/first.cpp
@@ -3,16 +3,24 @@ using namespace std;
 
 class Solution{
     public:
-        int findSum(int n){
+        int findSum(int n){                       // funfctional way of recursion
             if(n==0)return 0;
             else return n + findSum(n-1);
+        }
+        int findSum_Parameterize(int sum,int i){
+            if(i<1){
+                cout<<"sum is"<<sum;
+                return 0;
+            }
+            findSum_Parameterize(sum+i,i-1);
         }
 };
 
 int main(){
     // find sum of 10 numbers using recurson.
-    int n = 10;
+    int n = 5;
     int sum = 0;
     Solution obj;
-    cout<< obj.findSum(n);
+    // cout<< obj.findSum(n);
+    obj.findSum_Parameterize(sum,n);
 }
