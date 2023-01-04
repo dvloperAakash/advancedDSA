8d9115f9bb47dd9a7bf8f4a15884c8306310e420
Aakash Goswami <aakash.goswami2244@gmail.com>
2023-01-05 01:38:24 +0530

    add subsequncies problem sol

--- recursion/first.cpp
+++ recursion/first.cpp
@@ -2,17 +2,31 @@
 using namespace std;
 
 class Solution{
+    private:
+        int fibo_sum = 0;
     public:
         int findSum(int n){                       // functional way of recursion
             if(n==0)return 0;
             else return n + findSum(n-1);
         }
-        int findSum_Parameterize(int sum,int i){
+        int findSum_Parameterize(int sum,int i){   // parameterize way of recursion
             if(i<1){
                 cout<<"sum is"<<sum;
                 return 0;
             }
             findSum_Parameterize(sum+i,i-1);
+        }
+        void reverseArray(vector<int>&arr,int start,int end){
+            if(start>=end)return;
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            reverseArray(arr,start+1,end-1);
+        }
+        int fibonachi(int n,int i,int j){
+            if(n<2)return fibo_sum;
+            fibo_sum +=i+j;
+            fibonachi(n-1,j,i+j);
         }
 };
 
@@ -20,7 +34,11 @@ int main(){
     // find sum of 10 numbers using recurson.
     int n = 5;
     int sum = 0;
+    vector<int>arr = {12,5,7,1,3,19};
     Solution obj;
     // cout<< obj.findSum(n);
-    obj.findSum_Parameterize(sum,n);
+    // obj.findSum_Parameterize(sum,n);
+    // obj.reverseArray(arr,0,arr.size()-1);
+    // for(auto &a:arr)cout<<a<<" ";
+    cout<<obj.fibonachi(n,0,1);
 }
---
+++ recursion/subsequences.cpp
@@ -0,0 +1,52 @@
+// pattern for finding subsequences
+
+// fun(ind,[]){
+//     if(ind>=n){
+//         print([])
+//         return;
+//     }
+//     [].add(arr[i]);
+//     fun(ind+1,[]);  // case of taking element
+//     [].remove(arr[i]); // backtracking
+//     fun(ind+1,[]); // case of non taking element
+// }
+#include <bits/stdc++.h>
+using namespace std;
+
+class Subsequencies
+{
+public:
+    vector<vector<int>> ans;
+    void printSubseq(vector<int> &arr, vector<int> &ds, int index)
+    {
+        if (index >= arr.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(arr[index]);
+        printSubseq(arr, ds, index + 1);
+        ds.pop_back();
+        printSubseq(arr, ds, index + 1);
+    }
+};
+
+int main()
+{
+    vector<int> arr = {3, 1, 2};
+    vector<int> ds;
+    Subsequencies sub;
+    sub.printSubseq(arr, ds, 0);
+    for (auto &vec : sub.ans)
+    {
+        for (int i = 0; i < vec.size(); i++)
+        {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// TC : O(2^N) * N (extra N for printing).
+// SC : O(N) 
